19c0a7e1a3b339a2af8e384692b615b04302294b
stoievyevhenii <46534687+stoievyevhenii@users.noreply.github.com>
2019-11-15 12:31:03 +0200

    Model for exercises

---
+++ app/src/main/java/com/stoiev/devcorner/HomeListItem.java
@@ -0,0 +1,28 @@
+package com.stoiev.devcorner;
+
+public class HomeListItem{
+    private String title;
+    private String author;
+    private String group;
+
+    public HomeListItem(){
+        // empty constructor for Firebase UI
+    }
+    public HomeListItem(String title, String group, String author){
+        this.title = title;
+        this.group = group;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+}
